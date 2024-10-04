521dbc857bc2de8d84ffdbbe011597747027adc1
unknown <jxwang4053@gmail.com>
2024-10-03 23:46:12 -0400

    test on videos

--- tools/test_adaptive_model.py
+++ tools/test_adaptive_model.py
@@ -1,11 +1,3 @@
-# ------------------------------------------------------------------------------
-# pose.pytorch
-# Copyright (c) 2018-present Microsoft
-# Licensed under The Apache-2.0 License [see LICENSE for details]
-# Written by Bin Xiao (Bin.Xiao@microsoft.com)
-# Modified by Xiaofei Huang (xhuang@ece.neu.edu) and Nihang Fu (nihang@ece.neu.edu)
-# ------------------------------------------------------------------------------
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -13,7 +5,6 @@ from __future__ import print_function
 import argparse
 import os
 import pprint
-
 import torch
 import torch.nn.parallel
 import torch.backends.cudnn as cudnn
@@ -21,62 +12,66 @@ import torch.optim
 import torch.utils.data
 import torch.utils.data.distributed
 import torchvision.transforms as transforms
-
 import _init_paths
 from config import cfg
 from config import update_config
 from core.loss import JointsMSELoss
-#from core.function import validate
-from core.function import validate_feature
 from utils.utils import create_logger
-from pathlib import Path
-
 import dataset
 import models
 import numpy as np
-
+import cv2
+from tqdm import tqdm
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Train keypoints network')
-    # general
-    parser.add_argument('--cfg',
-                        help='experiment configure file name',
-                        required=True,
-                        type=str)
-
-    parser.add_argument('opts',
-                        help="Modify config options using the command-line",
-                        default=None,
-                        nargs=argparse.REMAINDER)
-
-    parser.add_argument('--modelDir',
-                        help='model directory',
-                        type=str,
-                        default='')
-    parser.add_argument('--logDir',
-                        help='log directory',
-                        type=str,
-                        default='')
-    parser.add_argument('--dataDir',
-                        help='data directory',
-                        type=str,
-                        default='')
-    parser.add_argument('--prevModelDir',
-                        help='prev Model directory',
-                        type=str,
-                        default='')
-
+    parser = argparse.ArgumentParser(description='Apply HRNet-FIDIP to video')
+    parser.add_argument('--cfg', help='experiment configure file name', required=True, type=str)
+    parser.add_argument('--videoPath', help='path to input video', required=True, type=str)
+    parser.add_argument('--outputPath', help='path to output video', required=True, type=str)
+    parser.add_argument('opts', help="Modify config options using the command-line", default=None, nargs=argparse.REMAINDER)
     args = parser.parse_args()
     return args
 
+def process_frame(model, frame, transform):
+    frame_tensor = transform(frame).unsqueeze(0).cuda()
+    with torch.no_grad():
+        output = model(frame_tensor)
+    # Process the output as needed
+    # This is a placeholder - modify according to your model's output
+    processed_frame = output[0].cpu().numpy().transpose(1, 2, 0)
+    processed_frame = (processed_frame * 255).astype(np.uint8)
+    return processed_frame
+
+def process_video(model, video_path, output_path, transform):
+    cap = cv2.VideoCapture(video_path)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
+    
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    
+    for _ in tqdm(range(total_frames)):
+        ret, frame = cap.read()
+        if not ret:
+            break
+        
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        processed_frame = process_frame(model, frame_rgb, transform)
+        processed_frame_bgr = cv2.cvtColor(processed_frame, cv2.COLOR_RGB2BGR)
+        
+        out.write(processed_frame_bgr)
+    
+    cap.release()
+    out.release()
 
 def main():
     args = parse_args()
     update_config(cfg, args)
 
-    logger, final_output_dir, tb_log_dir = create_logger(
-        cfg, args.cfg, 'valid')
-
+    logger, final_output_dir, tb_log_dir = create_logger(cfg, args.cfg, 'valid')
     logger.info(pprint.pformat(args))
     logger.info(cfg)
 
@@ -85,53 +80,30 @@ def main():
     torch.backends.cudnn.deterministic = cfg.CUDNN.DETERMINISTIC
     torch.backends.cudnn.enabled = cfg.CUDNN.ENABLED
 
-    model_p, model_d = eval('models.'+cfg.MODEL.NAME+'.get_adaptive_pose_net')(
-        cfg, is_train=False
-    )
+    model = eval('models.'+cfg.MODEL.NAME+'.get_adaptive_pose_net')(cfg, is_train=False)
 
     if cfg.TEST.MODEL_FILE:
         logger.info('=> loading model from {}'.format(cfg.TEST.MODEL_FILE))
-        model_p.load_state_dict(torch.load(cfg.TEST.MODEL_FILE), strict=False)
+        model.load_state_dict(torch.load(cfg.TEST.MODEL_FILE), strict=False)
     else:
-        model_state_file = os.path.join(
-            final_output_dir, 'final_state.pth'
-        )
+        model_state_file = os.path.join(final_output_dir, 'final_state.pth')
         logger.info('=> loading model from {}'.format(model_state_file))
-        model_p.load_state_dict(torch.load(model_state_file))
+        model.load_state_dict(torch.load(model_state_file))
 
-    model_p = torch.nn.DataParallel(model_p, device_ids=cfg.GPUS).cuda()
+    model = torch.nn.DataParallel(model, device_ids=cfg.GPUS).cuda()
 
-    # define pose loss function (criterion) and optimizer
-    criterion_p = JointsMSELoss(
-        use_target_weight=cfg.LOSS.USE_TARGET_WEIGHT
-    ).cuda()
-
-    # Data loading code
+    # Data transform
     normalize = transforms.Normalize(
         mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
     )
-    valid_dataset = eval('dataset.'+cfg.DATASET.DATASET)(
-        cfg, cfg.DATASET.ROOT, cfg.DATASET.TEST_SET, False,
-        transforms.Compose([
-            transforms.ToTensor(),
-            normalize,
-        ])
-    )
-    valid_loader = torch.utils.data.DataLoader(
-        valid_dataset,
-        batch_size=cfg.TEST.BATCH_SIZE_PER_GPU*len(cfg.GPUS),
-        shuffle=False,
-        num_workers=cfg.WORKERS,
-        pin_memory=True
-    )
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        normalize,
+    ])
 
-    # evaluate on validation set
-    _, feature_output, _ = validate_feature(cfg, valid_loader, valid_dataset, model_p, criterion_p,
-             final_output_dir, tb_log_dir)
-             
-    feature_file = os.path.join(final_output_dir, 'feature.npy')
-    np.save(feature_file, feature_output)
-             
+    # Process video
+    process_video(model, args.videoPath, args.outputPath, transform)
 
 if __name__ == '__main__':
     main()
+    
